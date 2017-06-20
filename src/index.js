ae2a5bd8b79ff7feba5d359ab19eb702f679bf96
Alex Anderson <anderson@medicmobile.org>
2017-06-20 11:14:57 +0000

    Check for changes at startup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const PouchDB = require('pouchdb');
 const lockfile = require('lockfile');
 
-const COUCH_URL = 'http://localhost:5984/medic';
+const LOCK_FILE = 'horticulturalist.lock';
+
+const COUCH_URL = 'http://admin:pass@localhost:5984/medic';
 const DDOC = '_design/medic';
 const APPS = [ 'medic-api', 'medic-sentinel' ];
 
@@ -11,51 +13,78 @@ if(lockFileExists()) {
   throw new Error('Lock file already exists.  Cannot start horticulturalisting.');
 }
 
-new PouchDB(COUCH_URL)
-  .changes({
-    live: true,
-    since: 'now',
-    doc_ids: [ DDOC ],
-    timeout: false,
-  })
-  .on('change', change => {
-    const ddoc = change.changes[0].doc;
-    const changedApps = getChangedApps(ddoc);
-    if(changedApps.length) {
-      waitForLock()
-        .then(() => unzipChangedApps(changedApps))
-        .then(() => stopApps())
-        .then(() => updateSymLinks(changedApps))
-        .then(() => startApps())
-        .then(() => cleanUpOldApps(ddoc));
-    }
-  })
-  .on('error', err => { throw err; });
+const db = new PouchDB(COUCH_URL);
 
-const cleanUpOldApps = ddoc =>
-  Promise.all(ddoc.node_modules.map(module => {
-    // TODO delete app dirs which do not match the current digest
-  }));
+db.get(DDOC)
+  .then(processDdoc)
+  .then(() => {
+    db
+      .changes({
+        live: true,
+        since: 'now',
+        doc_ids: [ DDOC ],
+        include_docs: true,
+        timeout: false,
+      })
+      .on('change', change => {
+        console.log('change listener fired for', JSON.stringify(Object.keys(change)));
+        processDdoc(change.doc);
+      })
+      .on('error', fatality);
+  })
+  .catch(fatality);
 
-function lockFileExists() {
-  return lockFile.checkSync(LOCK_FILE);
+function fatality(err) {
+  console.error(err);
+  process.exit(1);
 }
 
-const waitForLock = () => new Promise((resolve, reject) => {
-  lockfile.lock(LOCK_FILE, err => {
-    if(err) reject(err);
-    else resolve();
+function processDdoc(ddoc) {
+  console.log('processDdoc()');
+
+  const changedApps = getChangedApps(ddoc);
+
+  console.log('processDdoc()', 'changed:', changedApps);
+
+  if(changedApps.length) {
+    waitForLock()
+      .then(() => unzipChangedApps(changedApps))
+      .then(() => stopApps())
+      .then(() => updateSymLinks(changedApps))
+      .then(() => startApps())
+      .then(() => cleanUpOldApps(ddoc))
+      .catch(fatality);
+  }
+}
+
+const cleanUpOldApps = ddoc =>
+  Promise.all(ddoc.node_modules
+    .split(',')
+    .map(module => {
+      // TODO delete app dirs which do not match the current digest
+    }));
+
+function lockFileExists() {
+  return lockfile.checkSync(LOCK_FILE);
+}
+
+const waitForLock = () =>
+  new Promise((resolve, reject) => {
+    lockfile.lock(LOCK_FILE, err => {
+      if(err) reject(err);
+      else resolve();
+    });
   });
-});
 
 const getChangedApps = ddoc =>
   ddoc.node_modules
-    .map(moduleToApp)
+    .split(',')
+    .map(module => moduleToApp(ddoc, module))
     .filter(appAlreadyUnzipped);
 
-const moduleToApp = module =>
+const moduleToApp = (ddoc, module) =>
   ({
-    name: appNameFromNodeModule(module),
+    name: appNameFromModule(module),
     attachmentName: module,
     digest: ddoc._attachments[module].digest,
   });
@@ -82,8 +111,11 @@ const startApps = () =>
       (p, app) => p.then(exec(`svc-start ${app}`)),
       Promise.resolve);
 
-const exec = cmd => new Promise((resolve, reject) =>
-  child_process.exec(cmd, err => {
-    if(err) reject(err);
-    else resolve(err);
-  }));
+const exec = cmd =>
+  new Promise((resolve, reject) =>
+    child_process.exec(cmd, err => {
+      if(err) reject(err);
+      else resolve(err);
+    }));
+
+const path = (app, version) => `${app}/${version}`;
