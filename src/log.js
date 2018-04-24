47a887a9204d0e3bda89f0d4b95f990db358239b
Stefan du Fresne <stefan@sdufresne.info>
2018-04-24 11:13:54 +0100

    Minor logging changes
    We might back away from default verbose later, but for now let's
    go with this so people can feel confident about what is going on

--- src/log.js
+++ src/log.js
@@ -1,7 +1,10 @@
 const debug = require('debug');
 
 module.exports.debug = debug('horti:debug');
-module.exports.info = debug('horti:info');
-module.exports.warn = debug('horti:warn');
-module.exports.error = debug('horti:error');
 module.exports.stage = debug('horti:stage');
+module.exports.info = console.log;
+module.exports.error = console.error;
+
+if (!process.env.TESTING) {
+  debug.enable('horti:*');
+}
