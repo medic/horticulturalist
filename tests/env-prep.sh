118cff6128fd16c8e49200bf9365176aa02ee168
Stefan du Fresne <stefan@sdufresne.info>
2018-07-12 13:02:02 +0545

    debuggin'

--- tests/env-prep.sh
+++ tests/env-prep.sh
@@ -10,10 +10,10 @@ echo "CouchDB Started"
 # The next bit is pulled from our instructions: it should be updated
 # if those instructions are
 
-curl -X PUT http://localhost:5984/_config/admins/admin -d '"pass"'
-curl -X PUT http://admin:pass@localhost:5986/_config/chttpd/require_valid_user \
+curl -v -X PUT http://localhost:5986/_config/admins/admin -d '"pass"'
+curl -v -X PUT http://admin:pass@localhost:5986/_config/chttpd/require_valid_user \
   -d '"true"' -H "Content-Type: application/json";
-curl -X POST http://admin:pass@localhost:5984/_users \
+curl -v -X POST http://admin:pass@localhost:5984/_users \
   -H "Content-Type: application/json" \
   -d '{"_id": "org.couchdb.user:admin", "name": "admin", "password":"pass", "type":"user", "roles":[]}'
 
